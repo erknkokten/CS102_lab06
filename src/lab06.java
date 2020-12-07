a80af55c65acfbb5496d74a7af51408685da9f9e
erknkokten <erknkokten@gmail.com>
2020-12-07 23:30:18 +0300

    second part is done

--- src/lab06.java
+++ src/lab06.java
@@ -7,7 +7,11 @@ public class lab06 {
         String str;
         System.out.print("Length will be counted:");
         str = scanner.nextLine();
-        System.out.print(countLength(str));
+        System.out.println(countLength(str));
+
+        System.out.print("Non-Vowels will be counted:");
+        str = scanner.nextLine();
+        System.out.println(findNonVowels(str));
     }
 
     public static int countLength(String str) {
@@ -16,7 +20,19 @@ public class lab06 {
         }
         else
             return 1 + countLength(str.substring(1));
-
-
+    }
+    public static int findNonVowels(String str) {
+        str = str.toLowerCase();
+        if(str.length() == 0){
+            return 0;
+        }
+        else if(!(str.charAt(0) == 'a' ||str.charAt(0) == 'e'||
+                str.charAt(0) == 'u'||  str.charAt(0) == 'ı'
+                || str.charAt(0) == 'o'|| str.charAt(0) == 'ü'
+                || str.charAt(0) == 'i'||  str.charAt(0) == 'ö'
+                || str.charAt(0) == ' ' || Character.isDigit(str.charAt(0))))
+            return 1 + findNonVowels(str.substring(1));
+        else
+            return 0 + findNonVowels(str.substring(1));
     }
 }
